a6403a257ca1454f5139b6f53df43d14c50778de
msalejandrojose <msalejandrojose7@gmail.com>
2024-01-28 13:08:02 +0100

    creating env variables

--- dist/main.js
+++ dist/main.js
@@ -2,7 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const fs = require("fs");
+const dotenv = require("dotenv");
 async function bootstrap() {
+    if (fs.existsSync('.env')) {
+        dotenv.config();
+        process.env.DATABASE_URL = "mysql://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@" + process.env.DB_HOST + ":" + process.env.DB_PORT + "/" + process.env.DB_SCHEMA + "";
+    }
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     await app.listen(3000);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+
+  // Cargar las variables de entorno del archivo .env si existe
+  if (fs.existsSync('.env')) {
+    dotenv.config();
+    process.env.DATABASE_URL = "mysql://"+process.env.DB_USER+":"+process.env.DB_PASS+"@"+process.env.DB_HOST+":"+process.env.DB_PORT+"/"+process.env.DB_SCHEMA+"";
+  }
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
